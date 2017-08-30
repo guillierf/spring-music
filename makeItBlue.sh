f6fec93abc41c3ac40dd4fdb1433cb4e6518decc
guillierf <guillierf@vmware.com>
2017-08-29 21:41:22 -0700

    making banner blue

--- makeItBlue.sh
+++ makeItBlue.sh
@@ -4,7 +4,12 @@ git pull
 
 cp templates/blue-app.css src/main/resources/static/css/app.css
 git add src/main/resources/static/css/app.css
+git add makeItBlue.sh
 git commit -m 'making banner blue'
 git push -u origin master
 sleep 3
+echo " ****************************"
+echo " press any button to continue"
+echo " ****************************"
+read
 watch -n1 kubectl get all -o wide

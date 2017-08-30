a262a64c3a98a0208021a10117b553f6d87f907a
guillierf <guillierf@vmware.com>
2017-08-29 22:42:52 -0700

    updating buildsh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 now=$(date +"%s")
 
-#docker login 10.40.206.145 -u admin -p 'VMware1!'
+docker login 10.40.206.145 -u admin -p 'VMware1!'
 docker build -t 10.40.206.145/library/spring-music:"$now" .
 docker push 10.40.206.145/library/spring-music:"$now"
 

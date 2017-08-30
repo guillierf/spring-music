95ffe687b360fb2d3a6c8ae470cd59859568fe63
guillierf <guillierf@vmware.com>
2017-08-29 23:14:24 -0700

    changed makeItGreen.sh

--- makeItGreen.sh
+++ makeItGreen.sh
@@ -7,4 +7,8 @@ git add src/main/resources/static/css/app.css
 git commit -m 'making banner green'
 git push -u origin master
 sleep 3
+echo " ****************************"
+echo " press any button to continue"
+echo " ****************************"
+read
 watch -n1 kubectl get all -o wide
